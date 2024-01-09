4b37a2cd729c52fa9451187b044394b16e2413d5
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-01-09 13:42:15 +0100

    add: km prompt

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,6 @@
 // chiedere all'utente il numero di chilometri che vuole percorrere. 
+const km =parseInt (prompt ('quanti chilometri vuoi viaggiare?')) 
+console.log (km)
 // chiedere all'utente l'età del passeggero.
 // calcolare il prezzo totale del viaggio (0.21 € al km).
 
