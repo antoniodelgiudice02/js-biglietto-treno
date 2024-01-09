241613c7c5c624159669542495a426efadd705b1
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-01-09 13:53:18 +0100

    add: human Price

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,15 @@
 // chiedere all'utente il numero di chilometri che vuole percorrere. 
-const km =parseInt (prompt ('quanti chilometri vuole percorrere il passeggero?')) 
+const km = parseInt (prompt ('quanti chilometri vuole percorrere il passeggero?')) 
 console.log (km)
 
 // chiedere all'utente l'età del passeggero.
-const age =parseInt (prompt ('quanti anni ha il passeggero?')) 
+const age = parseInt (prompt ('quanti anni ha il passeggero?')) 
 console.log (age)
 
 // calcolare il prezzo totale del viaggio (0.21 € al km).
+const price = (km * 0.21)
+const humanPrice = parseInt(operation.toFixed(2))
+console.log (humanPrice)
 
 // applicare sconto del 20% per i minorenni.
 // applicare sconto del 40% per gli over 65.
