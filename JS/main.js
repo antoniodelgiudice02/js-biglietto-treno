047c389a6260247549a3045a74c5ddde730ea8bc
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-01-09 13:36:47 +0100

    first commit

---
+++ JS/main.js
@@ -0,0 +1,7 @@
+// chiedere all'utente il numero di chilometri che vuole percorrere. 
+// chiedere all'utente l'età del passeggero.
+// calcolare il prezzo totale del viaggio (0.21 € al km).
+
+// applicare sconto del 20% per i minorenni.
+// applicare sconto del 40% per gli over 65.
+// stampare prezzo finale in forma umana (con massimo due decimali).
